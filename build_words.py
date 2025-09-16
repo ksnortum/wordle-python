e81f2f2f58901e4be5f689bb6f86baf807ea60bc
Knute Snortum <knute@snortum.net>
2025-09-16 09:03:00 -0700

    Ask for new dict path; find words with upper-case letters

--- build_words.py
+++ build_words.py
@@ -4,15 +4,20 @@
 import re
 
 # Enter text dictionary
-path = input("Text dictionary of words to build from [/usr/share/dict/words]: ")
-if path == "":
-    path = '/usr/share/dict/words'
+path_to_dict = input("Text dictionary of words to build from [/usr/share/dict/words]: ")
+if path_to_dict == "":
+    path_to_dict = '/usr/share/dict/words'
+
+# Enter output file name
+path_to_new_dict = input("Output file name [words.txt]: ")
+if path_to_new_dict == "":
+    path_to_new_dict = 'words.txt'
 
 # Read all lines into a list
-print("Getting words from", path)
+print("Getting words from", path_to_dict)
 words = []
-with open(path, 'r', encoding='utf-8') as f_dictionary:
-    for line in f_dictionary.readlines():
+with open(path_to_dict, 'r', encoding='utf-8') as file:
+    for line in file.readlines():
         words.append(line)
 
 # Words we never want to show up in the dictionary:
@@ -22,7 +27,7 @@ sensitive_words = ["fucks", "shits", "cunts", "spiks", "spics", "spick", "kikes"
 # ... and probably a lot more I can't think of
 
 # Only five English letters and a LF (how to remove plurals?)
-valid = re.compile(r"^[a-z]{5}\n$")
+valid = re.compile(r"^[A-Za-z]{5}\n$")
 new_words = []
 for word in words:
     if valid.match(word) and word.rstrip() not in sensitive_words:
@@ -33,8 +38,7 @@ print("New size:", len(new_words))
 print("Writing new file...")
 
 # Write back to words file
-path = 'words.txt'
-with open(path, 'w', encoding='utf-8') as f_dictionary:
-    f_dictionary.writelines(new_words)
+with open(path_to_new_dict, 'w', encoding='utf-8') as file:
+    file.writelines(new_words)
 
 print("fin")
